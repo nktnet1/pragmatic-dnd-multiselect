84067ed6f9e57e840e5ca0cf9c8b7de680e106ed
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-01-03 20:00:24 +1100

    fix: base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pragmatic-dnd-multiselect.git',
+  base: '/pragmatic-dnd-multiselect',
   build: {
     chunkSizeWarningLimit: 800,
   }
